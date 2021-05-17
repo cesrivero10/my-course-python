eee789a196ffca847cdefb0cb7ec79467994182d
Cesar Rivero <cesrivero10@gmail.com>
2021-05-17 13:42:46 -0400

    segundo commit del curso

---
+++ conditionals.py
@@ -0,0 +1,10 @@
+name = "Ryan"
+lastname = "Lasseter"
+
+if name == "Jhon":
+  if lastname == "Carter":
+    print("You are Jhon Carter")
+  else:
+    print("you are not Jhon Carter")
+else:
+  print("you are not Jhon")
---
+++ lists.py
@@ -0,0 +1,22 @@
+demo_list = [1, 'hello', 1.34, True, [1,2,3]]
+colors = ['red', 'green', 'blue', 'red']
+
+number_list = list((1, 2, 3, 4))
+# print(type(number_list))
+
+# r = list(range(1,100))
+# print(r)
+# print(len(demo_list))
+# print(colors[-2])
+
+# print(dir(colors))
+
+# colors.append('violet')
+# colors.extend(['violet', 'yellow'])
+# colors.extend(['pink', 'black'])
+
+colors.insert(1, 'violet')
+
+colors.sort(reverse=True)
+
+print(colors.count('red'))
