9cc24d069db8956ec4938ef1f48fce392e43a535
Cesar Rivero <cesrivero10@gmail.com>
2021-05-17 13:47:04 -0400

    tercer commit

---
+++ dictionaries.py
@@ -0,0 +1,22 @@
+product = {
+  "name": "book",
+  "quantity": 3,
+  "price": 4.99
+  }
+
+person = {
+  "first_name": "Ryan",
+  "last_name": "Ray"
+}
+
+print(person.items())
+person.clear()
+
+print(person)
+
+product = [
+  {"name": "book", "price": 10.99},
+  {"name": "laptop", "price": 1000}
+]
+
+print(product)
